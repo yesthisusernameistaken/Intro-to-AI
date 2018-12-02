5e511469afcccfb8f458df8dd583b3ff62e3d00a
yesthisusernameistaken <45125817+yesthisusernameistaken@users.noreply.github.com>
2018-12-02 13:30:14 +0100

    Added full map

--- Ncode1.py
+++ Ncode1.py
@@ -12,6 +12,7 @@ import signal
 #Long term to do
 #Ues the dif between  current and previous reading
 #Will be getting more analogue values so will need to speed up, slow down motors at smaller increments
+#If it detecs the line twice on one side, stop motor?
 
 #Done
 #Get it to really stop when it detecs a line
@@ -25,8 +26,8 @@ import signal
 #Find a way of setting a list of steps to take (ie turns) - dictionary
 
 #Threshold for the sensors, the two color ones and the light
-THRESHOLD_LEFT = 30
-THRESHOLD_RIGHT = 30
+THRESHOLD_LEFT = 25
+THRESHOLD_RIGHT = 25
 THRESHOLD_FRONT = 450
 THRESHOLD_DIF = 15
 
@@ -35,11 +36,12 @@ holdBit = 0
 ReversedBit = 0
 
 #Speeds the motors will use
-BASE_SPEED = 300
+BASE_SPEED = 100
 TURN_SPEED = 400
 TURN_SPEED_SENS = 700
 TURN_SPEED_SENS_FINAL = 300
 TURN_SPEED_SENS_U = 300
+BASE_SPEED_FORW = 500
 
 REVERSE_DIST = 100
 
@@ -149,7 +151,7 @@ def UturnSR():
     mB.polarity = "normal"
 
     #Now do the turn
-    mA.run_to_rel_pos(position_sp=350, speed_sp=TURN_SPEED_SENS, stop_action="hold")
+    mA.run_to_rel_pos(position_sp=400, speed_sp=TURN_SPEED_SENS, stop_action="hold")
     mB.polarity = "inversed"
     mB.run_to_rel_pos(position_sp=400, speed_sp=TURN_SPEED_SENS, stop_action="hold")
     mA.wait_while('running')
@@ -206,13 +208,23 @@ def TurnForLine(sensorRight, sensorLeft):
      mB.run_forever(speed_sp=TURN_SPEED)
  return
 
+def TurnForLineReverse(sensorRight, sensorLeft):
+ if sensorRight < sensorLeft:
+     print("Turn right")
+     mB.run_forever(speed_sp=TURN_SPEED)
+     mA.run_forever(speed_sp=BASE_SPEED)
+ else:
+     print("Turn left")
+     mB.run_forever(speed_sp=BASE_SPEED)
+     mA.run_forever(speed_sp=TURN_SPEED)
+ return
+
 #This bit attempts to count the black intersections
 def countLine(sensorFront):
     global holdBit
     global intersections
-    if holdBit == 0:
+    if holdBit == 1:
         intersections = intersections+1
-        holdBit = holdBit+1
     return
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -220,28 +232,158 @@ def countLine(sensorFront):
 
 def indirect(i):
         switcher={
-                0: Nothing,
-                1: ReverseM,
-                2: Nothing,
-                3: ReverseM,
-                4: leftTurnS,
-                5: leftTurnS,
-                6: leftTurnS,
-                7: Nothing,
-                8: UturnSR,
-                9: Nothing,
-                10: Nothing, 
-                11: Nothing,
-                12: leftTurnS,
-                13: leftTurnS,
-                14: leftTurnS,
-                15: leftTurnS,
-                16: leftTurnS,
-                17: leftTurnS,
-                18: leftTurnS,
-                19: leftTurnS,
-                20: leftTurnS,
-                21: UturnSR  }
+            0 : Nothing ,
+            1 : rightTurnS ,
+            2 : Nothing ,
+            3 : Nothing ,
+            4 : Nothing ,
+            5 : rightTurnS ,
+            6 : Nothing ,
+            7 : Nothing ,
+            8 : Nothing ,
+            9 : rightTurnS ,
+            10 : Nothing ,
+            11 : Nothing ,
+            12 : UturnSR ,
+            13 : leftTurnS ,
+            14 : leftTurnS ,
+            15 : Nothing ,
+            16 : Nothing ,
+            17 : Nothing ,
+            18 : UturnSR ,
+            19 : leftTurnS ,
+            20 : leftTurnS ,
+            21 : Nothing ,
+            22 : Nothing ,
+            23 : Nothing ,
+            24 : UturnSR ,
+            25 : leftTurnS ,
+            26 : leftTurnS ,
+            27 : Nothing ,
+            28 : UturnSR ,
+            29 : Nothing ,
+            30 : Nothing ,
+            31 : rightTurnS ,
+            32 : Nothing ,
+            33 : Nothing ,
+            34 : rightTurnS ,
+            35 : Nothing ,
+            36 : Nothing ,
+            37 : leftTurnS ,
+            38 : Nothing ,
+            39 : leftTurnS ,
+            40 : leftTurnS ,
+            41 : Nothing ,
+            42 : UturnSR ,
+            43 : rightTurnS ,
+            44 : rightTurnS ,
+            45 : Nothing ,
+            46 : Nothing ,
+            47 : UturnSR ,
+            48 : leftTurnS ,
+            49 : leftTurnS ,
+            50 : Nothing ,
+            51 : Nothing ,
+            52 : Nothing ,
+            53 : UturnSR ,
+            54 : leftTurnS ,
+            55 : leftTurnS ,
+            56 : Nothing ,
+            57 : UturnSR ,
+            58 : rightTurnS ,
+            59 : rightTurnS ,
+            60 : Nothing ,
+            61 : UturnSR ,
+            62 : leftTurnS ,
+            63 : leftTurnS ,
+            64 : Nothing ,
+            65 : Nothing ,
+            66 : UturnSR ,
+            67 : Nothing ,
+            68 : rightTurnS ,
+            69 : Nothing ,
+            70 : Nothing ,
+            71 : Nothing ,
+            72 : Nothing ,
+            73 : rightTurnS ,
+            74 : Nothing ,
+            75 : Nothing ,
+            76 : leftTurnS ,
+            77 : Nothing ,
+            78 : Nothing ,
+            79 : leftTurnS ,
+            80 : Nothing ,
+            81 : Nothing ,
+            82 : Nothing ,
+            83 : leftTurnS ,
+            84 : leftTurnS ,
+            85 : Nothing ,
+            86 : UturnSR ,
+            87 : rightTurnS ,
+            88 : Nothing ,
+            89 : rightTurnS ,
+            90 : Nothing ,
+            91 : Nothing ,
+            92 : Nothing ,
+            93 : UturnSR ,
+            94 : rightTurnS ,
+            95 : rightTurnS ,
+            96 : Nothing ,
+            97 : Nothing ,
+            98 : UturnSR ,
+            99 : leftTurnS ,
+            100 : leftTurnS ,
+            101 : Nothing ,
+            102 : Nothing ,
+            103 : Nothing ,
+            104 : UturnSR ,
+            105 : rightTurnS ,
+            106 : rightTurnS ,
+            107 : Nothing ,
+            108 : UturnSR ,
+            109 : rightTurnS ,
+            110 : rightTurnS ,
+            111 : Nothing ,
+            112 : UturnSR ,
+            113 : leftTurnS ,
+            114 : Nothing ,
+            115 : Nothing ,
+            116 : Nothing ,
+            117 : rightTurnS ,
+            118 : Nothing ,
+            119 : leftTurnS ,
+            120 : Nothing ,
+            121 : Nothing ,
+            122 : leftTurnS ,
+            123 : Nothing ,
+            124 : leftTurnS ,
+            125 : leftTurnS ,
+            126 : Nothing ,
+            127 : UturnSR ,
+            128 : rightTurnS ,
+            129 : rightTurnS ,
+            130 : Nothing ,
+            131 : Nothing ,
+            132 : Nothing ,
+            133 : UturnSR ,
+            134 : rightTurnS ,
+            135 : rightTurnS ,
+            136 : Nothing ,
+            137 : rightTurnS ,
+            138 : leftTurnS ,
+            139 : leftTurnS ,
+            140 : Nothing ,
+            141 : Nothing ,
+            142 : leftTurnS ,
+            143 : rightTurnS ,
+            144 : Nothing ,
+            145 : rightTurnS ,
+            146 : Nothing ,
+            147 : rightTurnS ,
+            148 : rightTurnS ,
+            149 : rightTurnS ,
+            150 : leftTurnS ,
+            151 : leftTurnS, }
         func=switcher.get(i, "Invalid")
         return func()
 
@@ -249,51 +391,18 @@ def indirect(i):
 #----------------------------------------------------------------------------------------------------------------------
 #This would be the main bit
 
-def followLinev2():
-    global holdBit
-    global intersections
-    #Read the sensor value and store it
-    sensorLeft = colorSensorLeft.value()
-    sensorRight = colorSensorRight.value()
-    sensorFront = lightSensorFront.value()	
-    print("-------------------------------------------------------------------------------------------")
-    #Print the sensor values to the terminal (for debugging reasons, like a lot of the prints here)
-    print("Left: ", sensorLeft, "Right: ", sensorRight, "Front: ", sensorFront)
-    print("Number of intersections counted = ", intersections)
-
-    #Check the front sensor and stop/count it, if a line is detected
-    if sensorFront < THRESHOLD_FRONT:
-        #Black line detected by front sensor
-        countLine(sensorFront)
-        indirect(intersections)
-    else:
-        #Front sensor doesn't see a black line
-        holdBit = 0
-        #Get the difference between the two sensors
-        sensorDif = sensorLeft-sensorRight
-        sensorDif = abs(sensorDif)
-        print("Sensor difference: ", sensorDif)
-
-        if sensorDif < THRESHOLD_DIF:
-            print("Go ahead, difference is sensors is below threshold")
-            mA.run_forever(speed_sp=BASE_SPEED)
-            mB.run_forever(speed_sp=BASE_SPEED)
-        else:
-            print("There is a diff in the sensors readings above the threshold")
-            TurnForLine(sensorRight, sensorLeft)
-            #one might be looking at black	
-    
-
-    return
-
 def followLinev3():
     global holdBit
     global intersections
     global ReversedBit
-
+    print ("Reversed bit:", ReversedBit)
     print("-------------------------------------------------------------------------------------------")
     sensorFront = lightSensorFront.value()
     
+    #sensorLeft = colorSensorLeft.value()
+    #sensorRight = colorSensorRight.value()
+    #print("Left: ", sensorLeft, "Right: ", sensorRight, "Front: ", sensorFront)	
+
     if ReversedBit == 0:
         #Normal operation forward
         sensorLeft = colorSensorLeft.value()
@@ -310,8 +419,10 @@ def followLinev3():
     #Check the front sensor and stop/count it, if a line is detected
     if sensorFront < THRESHOLD_FRONT:
         #Black line detected by front sensor
+        holdBit = holdBit+1
         countLine(sensorFront)
         indirect(intersections)
+        
     else:
         #Front sensor doesn't see a black line
         holdBit = 0
@@ -322,13 +433,18 @@ def followLinev3():
 
         if sensorDif < THRESHOLD_DIF:
             print("Go ahead, difference is sensors is below threshold")
-            mA.run_forever(speed_sp=BASE_SPEED)
-            mB.run_forever(speed_sp=BASE_SPEED)
+            mA.run_forever(speed_sp=BASE_SPEED_FORW)
+            mB.run_forever(speed_sp=BASE_SPEED_FORW)
         else:
             print("There is a diff in the sensors readings above the threshold")
-            TurnForLine(sensorRight, sensorLeft)
-            #one might be looking at black	
-    
+            if ReversedBit == 0:
+                #Normal operation
+                TurnForLine(sensorRight, sensorLeft)
+            else:
+                print("Reversed sensor input to motor")
+                #TurnForLine(sensorLeft, sensorRight)
+                TurnForLineReverse(sensorRight, sensorLeft)
+
 
     return
 
